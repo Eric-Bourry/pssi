eede413bf2c1ef4ab22abbd3d0348fd87e84deb8
julien.flaissy@gmail.com <julien.flaissy@gmail.com@f9924172-da35-2c64-8ab5-61330d39ffcb>
2010-09-25 12:42:50 +0000

    display: minor modifications, comments.

--- trunk/display.py
+++ trunk/display.py
@@ -1,43 +1,44 @@
 # -*- coding: utf-8 -*-
 
 from smartcard.util import toHexString
-
+import types
 import sys
 
 verboseMode = False
 
 def formatOutput(interpretation, rawdata, comment):
+    """Returns a string with the value of a field, its interpretation
+    and a comment in verbose mode."""
     global verboseMode
     if verboseMode:
-        return ("%-35s" % interpretation)+" ---   "+str(rawdata)+" ("+comment+")"
-    return ("%-35s" % interpretation)+" ("+comment+")"
+        return "%-35s ---   %s (%s)" % (interpretation, rawdata, comment)
+    return "%-35s (%s)" % (interpretation, comment)
 
 # TODO : également dans les interpréteurs ?
 # TODO : Faire des fonctions dispo aux interpréteurs, du genre matchWithCode
 def hexListToBinaryString(tab):
-    """retourne la chaine de la representation binaire de tab"""
+    """Returns a string containing the binary representation of `tab'."""
     s = ''
     blen = len(tab) * 8
-    for b in range(0,blen):
+    for b in range(0, blen):
         s = s + "%d" % (((tab[b/8] >> ((7-b)%8))) & 1)
     return s
 
-
-# FIXME: nom pourri
+# TODO(m): nom pourri
 def printExchange(query, response, sw1, sw2):
-    """Affiche un échange query-response."""
+    """Prints an APDU exchange (query-response)."""
     print ">> ",
-    if type(query) == type([]):
+    if type(query) is types.ListType:
          print toHexString(query)
     else:
         print query
     print "<< ", toHexString(response), " / ", "%x %x" % (sw1, sw2)
 
 def printExchangeWithBinary(query, response, sw1, sw2):
-    """Affiche un échange query-response, avec en plus la reponse en binaire."""
+    """Prints an APDU exchange (query-response) in hexadecimal, and
+    the response in binary."""
     printExchange(query, response, sw1, sw2)
     print "\t ==  ", toBinaryString(response)
-
 
 def readPIN():
     print "PIN code required, please enter it:"
@@ -48,51 +49,47 @@ def readPIN():
         pin.append(ord(c))
     for i in range(len(code), 8):
         pin.append(0xff)
-    return pin 
-
+    return pin
 
 def errorPIN():
+    # TODO(e): pourquoi le \n ?
     print "Error while verifying the PIN code\n"
     sys.exit()
-
 
 def printAddress(address, space):
     length = len(address)
     print space, "[0x%02x%02x]" % (address[length-2], address[length-1])
-    
-    
+
 def printRecord(response, nb):
     print "====  ", nb, "  ===="
     print "\t", toBinaryString(response), "\n"
 
 
+# Main printing functions.
 def prettyPrint(data, tabs=""):
-    if type(data) == type(dict()):
+    """Pretty print to standard output."""
+    if type(data) is types.DictType:
         for key in data["Keys"]:
             print
-            if (type(key) == type("")):
+            if type(key) is types.StringType:
                 print tabs, ("%-27s" % key),
-                prettyPrint(data[key], tabs+"   ")
+                prettyPrint(data[key], tabs + "   ")
             else:
                 print tabs, "====  ", key, "  ====",
-                prettyPrint(data[key], tabs+"   ")
+                prettyPrint(data[key], tabs + "   ")
     else:
-        print ": "+data,
-                
-def prettyPrintToFile(data, file, tabs=""):
-    """Ecriture d'un dump sous forme lisible dans le fichier `file' (handle)."""
-    if type(data) == type(dict()):
+        print ": " + data,
+
+def prettyPrintToFile(data, output_file, tabs=""):
+    """Pretty print to `output_file' (handle)."""
+    if type(data) is types.DictType:
         for key in data["Keys"]:
-            file.write("\n")
-            if (type(key) == type("")):
-                file.write(tabs)
-                file.write(("%-27s" % key))
-                prettyPrintToFile(data[key], file, tabs+"   ")
+            output_file.write("\n")
+            if type(key) is types.StringType:
+                output_file.write("%s%-27s" % (tabs, key))
+                prettyPrintToFile(data[key], output_file, tabs+"   ")
             else:
-                file.write(tabs)
-                file.write("====  ")
-                file.write(str(key))
-                file.write("  ====")
-                prettyPrintToFile(data[key], file, tabs)
+                output_file.write("%s====  %s  ====" % (tabs, str(key)))
+                prettyPrintToFile(data[key], output_file, tabs)
     else:
-        file.write(": "+str(data))
+        output_file.write(": "+str(data))
