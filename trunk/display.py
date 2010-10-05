da835f560ae395ecf80a65669b8126055a1dbe72
eric.bourry@gmail.com <eric.bourry@gmail.com@f9924172-da35-2c64-8ab5-61330d39ffcb>
2010-10-05 19:56:10 +0000

    Details en relisant display.py

--- trunk/display.py
+++ trunk/display.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 
+# -- display.py
+# Output and formatting functions
+
+
 from smartcard.util import toHexString
 import types
 import sys
 
+# In verbose mode, we output the raw data
 verboseMode = False
 
 def formatOutput(interpretation, rawdata, comment):
@@ -41,6 +46,7 @@ def printExchangeWithBinary(query, response, sw1, sw2):
     print "\t ==  ", hexListToBinaryString(response)
 
 def readPIN():
+    """Asks the user for his PIN code, and returns it as a sequence of 8 bytes"""
     print "PIN code required, please enter it:"
     code = raw_input('--> ')
     code = code[0:8]
@@ -52,20 +58,21 @@ def readPIN():
     return pin
 
 def errorPIN():
-    # TODO(e): pourquoi le \n ?
-    print "Error while verifying the PIN code\n"
+    """Exits because of a wrong PIN code"""
+    print "Error while verifying the PIN code"
     sys.exit()
 
 def printAddress(address, space):
+    """Prints some spaces followed by the relative part of an address"""
     length = len(address)
     print space, "[0x%02x%02x]" % (address[length-2], address[length-1])
 
-def printRecord(response, nb):
+def printRecordInBinary(response, nb):
+    """Prints the content of a record in binary, along with its index"""
     print "====  ", nb, "  ===="
     print "\t", hexListToBinaryString(response), "\n"
 
 
-# Main printing functions.
 def prettyPrint(data, tabs=""):
     """Pretty print to standard output."""
     if type(data) is types.DictType:
